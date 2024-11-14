946d80695ccfce3e45ffa4faa697361ed4f07b1e
Anushka Nair <anushka.nair@sjsu.edu>
2024-11-13 18:18:58 -0800

    Updated main page

--- Welcome.py
+++ Welcome.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import time
 
-st.markdown("## Welcome to the Parks & Recreational Center Assistant!")
+st.markdown("## Welcome to ParkPal!")
 st.sidebar.markdown("# Welcome Page")
 
 message = "#### If you are someone who enjoys the outdoors and is located in San Jose, I can help! "
@@ -50,13 +50,16 @@ park_video(option)
 st.divider()
 
 st.subheader('Additional Assisant Features', divider='grey')
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4 = st.columns(4)
 with col1:
-   st.page_link("main_page.py", label="Welcome", disabled=True)
+   st.page_link("Welcome.py", label="Welcome", disabled=True)
 
 with col2:
-   st.page_link("pages/assistant_st.py", label="Assistant")
+   st.page_link("pages/Make_Reservation.py", label="Make Reservation")
 
 with col3:
-   st.page_link("pages/vision.py", label="Vision")
+   st.page_link("pages/ParkPal_Chatbot.py", label="ParkPal Chatbot")
+
+with col4:
+   st.page_link("pages/Upload_Park_Sign.py", label="Upload Park Sign")
 
